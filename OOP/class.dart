f905f7b3e39debd99f42ad3e35135d9c0f755bb0
anakin-dabir <anakindabir@gmail.com>
2023-06-13 01:16:31 +0500

    Constructor

---
+++ OOP/class.dart
@@ -0,0 +1,11 @@
+class Teacher {
+  final String _name;
+  final int _age;
+
+  // Teacher(this._name, this._age);
+  Teacher(String name, int age)
+      : _name = name,
+        _age = age;
+}
+
+void main() {}
